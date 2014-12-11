d4957ea382ccada8a0b12db631f3199e94a40659
mroach <mroach@mroach.com>
2014-12-11 21:46:37 +0800

    Disable admin area outside of development for now

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,3 +1,10 @@
 class Admin::AdminController < ApplicationController
   layout 'admin'
+
+  # Disable the admin area for now
+  before_action :reject
+
+  def reject
+    raise ActionController::RoutingError.new('Not Found') unless Rails.env.development?
+  end
 end
